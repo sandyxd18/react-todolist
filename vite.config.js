9418f67f5543b1a744ec326d12583a0bd2b85f1e
Sandy <sandyidn04@gmail.com>
2025-05-25 17:38:40 +0700

    update env

--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,11 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react-swc'
 import tailwindcss from '@tailwindcss/vite'
+import * as dotenv from 'dotenv'
+
+dotenv.config()
+
+const host = process.env.VITE_BASE_URL
 
 // https://vite.dev/config/
 export default defineConfig({
@@ -17,7 +22,7 @@ export default defineConfig({
     port: 5050,
     strictPort: true,
     host: true,
-    origin: 'http://0.0.0.0:5050',
+    origin: 'http://${host}',
     
   },
 })
